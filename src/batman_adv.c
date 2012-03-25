31b90785f9785497493dbbd8f7416576dd27af9d
Torsti Schulz <torsti.schulz@iki.fi>
2012-03-25 18:23:04 +0300

    Add struct, dummy function & comments for version handling

--- src/batman_adv.c
+++ src/batman_adv.c
@@ -22,6 +22,37 @@
 
 #include "batman_adv.h"
 
+/* We only support the "new" version numbering introduced with 2010.0.0. We
+ * don't see much reason to even trying to support older versions than that.
+ */
+
+typedef struct
+{
+	unsigned int year;
+	unsigned int release_number;
+	unsigned int bugfix_counter;
+} batman_adv_version;
+
+/* Releases to date. Last updated 2012-03-25.
+ *
+ * RELEASE  | DATE       | STATUS
+ * ---------|------------|-------
+ * 2010.0.0 | 2010-06-19 | ?
+ * 2010.1.0 | 2010-09-04 | ?
+ * 2010.2.0 | 2010-11-22 | ?
+ * 2011.1.0 | 2011-01-29 | ?
+ * 2011.2.0 | 2011-04-19 | ?
+ * 2011.3.0 | 2011-06-19 | ?
+ * 2011.3.1 | 2011-10-19 | ?
+ * 2011.4.0 | 2011-11-14 | partial
+ * 2012.0.0 | 2012-02-09 | ?
+ */
+
+static batman_adv_version available_batman_adv_versions[] = {
+	{0,    0, 0}, // dummy version: something is wrong or not implemented
+	{2011, 4, 0}
+};
+
 static int batman_adv_kernel_mod_loaded (void)
 {
 	if (access ("/sys/module/batman_adv/version", F_OK) != -1) {
@@ -35,4 +66,10 @@ static int batman_adv_kernel_mod_loaded (void)
 }
 
 // TODO: batman_adv_version: fp = fopen ("/sys/module/batman_adv/version");
+
+static batman_adv_version batman_adv_module_version (void)
+{
+	batman_adv_version version = {0, 0, 0};
+	return version;
+}
 
