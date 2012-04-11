787a68f0d44b32967c3a643fbff869793e5a9a33
Timo Sand <timo.sand@kapsi.fi>
2012-04-11 21:32:32 +0300

    ADDED: Tests for meshaware

---
+++ tests/cunit/meshaware_tests.c
@@ -0,0 +1,45 @@
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+
+#include <stdlib.h>
+
+#include "../../src/meshaware.h"
+
+void check_that_batman_adv_is_loaded(void) {
+	maw_mesh_protocol *returned_protocol = malloc(sizeof(maw_mesh_protocol));
+	maw_determine_mesh_protocol(returned_protocol);
+	CU_ASSERT (returned_protocol->name == batman_adv)
+}
+
+int main (void) {
+	unsigned int failures;
+	CU_pSuite pSuite = NULL;
+
+	if (CU_initialize_registry() != CUE_SUCCESS)
+		return CU_get_error();
+
+	pSuite = CU_add_suite ("meshaware suite", NULL, NULL);
+
+	if (!pSuite) {
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	if (!CU_add_test (pSuite,
+	                  "Test whether checking for mesh protocol works",
+	                  check_that_batman_adv_is_loaded)) {
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	CU_basic_set_mode (CU_BRM_VERBOSE);
+	CU_basic_run_tests();
+	failures = CU_get_number_of_failures();
+	CU_cleanup_registry();
+
+	if (failures > 0) {
+		return EXIT_FAILURE;
+	} else {
+		return EXIT_SUCCESS;
+	}
+}
